acbf88185e6cc70a2f47b86e1437054dae382fbb
unknown <renantielas@poli.ufrj.br>
2021-09-25 02:19:38 -0300

    teste env var

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ module.exports = {
       'drive.google.com'
       ],
     },
-   
+    env: {  PRIVATE_KEY: process.env.PRIVATE_KEY,
+      CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+          },
 
     async redirects() {
       return [
