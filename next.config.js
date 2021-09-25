be2cfa3c795231ca903628075f7f77a4e2a31468
unknown <renantielas@poli.ufrj.br>
2021-09-25 01:51:27 -0300

    teste env var

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ module.exports = {
       domains: [
       'drive.google.com'
       ],
-    },
+    }
+    ,plugins: [
+      new Dotenv({
+            systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      
+      })
+    ],
 
     async redirects() {
       return [
